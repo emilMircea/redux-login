8581554bd19f66441a5fb71227b0d834facb4739
emilMircea <emil-mircea@protonmail.com>
2018-09-14 17:03:57 +0200

    remove unnecessary imports and styles

--- src/App.js
+++ src/App.js
@@ -3,39 +3,17 @@ import './App.css'
 import Carousel from 'grommet/components/Carousel'
 import Box from 'grommet/components/Box'
 import Title from 'grommet/components/Title'
-import Form from 'grommet/components/Form'
-import { withStyles } from '@material-ui/core/styles'
 import UsernameInput from './components/inputs/UserNameInput'
 import PasswordInput from './components/inputs/PasswordInput'
 import LoginButton from './components/buttons/LoginButton'
 import ForgotPassButton from './components/buttons/ForgotPassButton'
-import Header from 'grommet/components/Header'
+
 import foto1 from './assets/images/foto-1.png'
 import foto2 from './assets/images/foto-2.jpg'
 
-const styles = theme => ({
-	container: {
-		display: 'flex',
-		flexWrap: 'wrap'
-	},
-	button: {
-    paddingTop: 20,
-    paddingBottom: 20,
-		paddingLeft: 40,
-		paddingRight: 40,
-    marginRight: 20,
-    marginTop: 30,
-		background: 'black',
-		color: 'white',
-  },
-	link: {
-		background: 'none',
-	}
-})
 
 class App extends Component {
 	render() {
-		const { classes } = this.props
 		return (
 			<div className="flex-container">
 				<div className="column one">
@@ -67,4 +45,4 @@ class App extends Component {
 	}
 }
 
-export default withStyles(styles)(App)
+export default App
