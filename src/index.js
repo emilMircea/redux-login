ad41315c3b29e7c2ffbc2827a2cde0440b787bed
emilMircea <emil-mircea@protonmail.com>
2018-09-15 11:06:28 +0200

    remove store and provider (redux) for the moment

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import store from './store/store'
 import 'grommet/grommet.min.css'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root'))
+  <App />,
+  document.getElementById('root')
+)
 registerServiceWorker()
