acfe40d55a8312af45143df9befcd3f76d54898c
emilMircea <emil-mircea@protonmail.com>
2018-09-15 11:10:58 +0200

    remove unnecessary code from the component

--- src/components/inputs/UserNameInput.js
+++ src/components/inputs/UserNameInput.js
@@ -1,60 +1,44 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import FormControl from '@material-ui/core/FormControl';
-
+import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Input from '@material-ui/core/Input'
+import InputLabel from '@material-ui/core/InputLabel'
+import FormControl from '@material-ui/core/FormControl'
 
 const styles = theme => ({
-  root: {
+	root: {
 		width: 400,
-    marginBottom: 20
-  },
-  cssLabel: {
-    '&$cssFocused': {
-      color: 'black',
-    },
-  },
-  cssFocused: {},
-  cssUnderline: {
-    '&:after': {
-      borderBottomColor: 'black',
-    },
-  }
-});
-
-
+		marginBottom: 20
+	},
+	cssLabel: {
+		'&$cssFocused': {
+			color: 'black'
+		}
+	},
+	cssFocused: {},
+	cssUnderline: {
+		'&:after': {
+			borderBottomColor: 'black'
+		}
+	}
+})
 
 function UserNameInput(props) {
-  const { classes } = props;
+	const { classes } = props
 
-  return (
-    <div className={classes.container}>
-      <FormControl className={classes.margin}>
-        <InputLabel
-          FormLabelClasses={{
-            root: classes.cssLabel,
-            focused: classes.cssFocused,
-          }}
-          htmlFor="custom-css-input"
-        >
-          Username
-        </InputLabel>
-        <Input
-          classes={{
-            root: classes.root,
-            underline: classes.cssUnderline,
-          }}
-          id="custom-css-input"
-        />
-      </FormControl>
-    </div>
-  );
+	return (
+			<Input
+				classes={{
+					root: classes.root,
+					underline: classes.cssUnderline
+				}}
+				id="custom-css-input"
+			/>
+	)
 }
 
 UserNameInput.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
+	classes: PropTypes.object.isRequired
+}
 
-export default withStyles(styles)(UserNameInput);
+export default withStyles(styles)(UserNameInput)
